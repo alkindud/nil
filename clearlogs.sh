204879c2c9c082414043460e243a2754175bbeb3
nil <44141464+alkindud@users.noreply.github.com>
2020-06-23 20:15:18 +0700

    Add files via upload

---
+++ clearlogs.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+NODE_USER=$(whoami)
+
+if [ "$(ps -e | grep sind | wc -l)" -eq "0" ]; then
+    exit 0
+fi
+
+sindirs=$(find /home/$NODE_USER -name '.sin*' -type d)
+for i in $sindirs; do
+    test -f $i/debug.log && echo -n > $i/debug.log
+done
