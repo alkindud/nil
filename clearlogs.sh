42b11c763325f0f6b91b8b265df158b579bccf92
nil <44141464+alkindud@users.noreply.github.com>
2020-11-10 21:13:41 +0700

    Update clearlogs.sh

--- clearlogs.sh
+++ clearlogs.sh
@@ -6,7 +6,7 @@ if [ "$(ps -e | grep sind | wc -l)" -eq "0" ]; then
     exit 0
 fi
 
-sindirs=$(find /home/$NODE_USER -name '.sin*' -type d)
+sindirs=$(find /home/$NODE_USER -maxdepth 1 -name '.sin*' -type d)
 for i in $sindirs; do
     test -f $i/debug.log && echo -n > $i/debug.log
 done
